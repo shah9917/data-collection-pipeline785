2ce7c0dc66309e412f2ee859a5969fc8d994410c
shah9917 <shahharsha9917@gmail.com>
2022-10-17 08:23:35 +0100

    Scraper class created

---
+++ scrape.py
@@ -0,0 +1,31 @@
+from selenium import webdriver
+import time 
+
+driver = webdriver.Chrome('./chromedriver')
+#driver.get("https://uk.trustpilot.com/")
+
+class Scraper:
+    def __init__(self, website):
+        self.website = website
+    
+    def get_website(self):
+        return driver.get(self.website)
+
+    def close(self):
+        driver.quit()
+
+    def ignore_cookie(self):
+        try:
+            driver.switch_to.frame('onetrust-consent-sdk')
+            ignore_cookies = driver.find_element(by = By.XPATH, value='//*[@id="onetrust-rehect-all-handler"]')
+            ignore_cookies.click()
+        except:
+            pass
+
+reviews = Scraper("https://uk.trustpilot.com/")
+reviews.get_website()
+time.sleep(2)
+reviews.ignore_cookie()
+time.sleep(2)
+reviews.close()
+
