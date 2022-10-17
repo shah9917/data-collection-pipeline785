39ff37248e04096771378615e9ccdec7169fdb58
shah9917 <shahharsha9917@gmail.com>
2022-10-17 10:14:12 +0100

    Scroll method added

--- scrape.py
+++ scrape.py
@@ -19,15 +19,20 @@ class Scraper:
     def ignore_cookie(self):
         try:
             driver.switch_to.frame('onetrust-consent-sdk')
-            ignore_cookies = driver.find_element(by = By.XPATH, value='//*[@id="onetrust-rehect-all-handler"]')
+            ignore_cookies = driver.find_element(by = By.XPATH, value='/html/body/div[3]/div[2]/div/div/div[2]/div/div/button[1]')
             ignore_cookies.click()
         except:
             pass
+    def scroll_end(self):
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 
 reviews = Scraper("https://uk.trustpilot.com/")
 reviews.get_website()
 time.sleep(2)
 reviews.ignore_cookie()
 time.sleep(2)
+reviews.scroll_end()
+time.sleep(2)
 reviews.close()
+
 
